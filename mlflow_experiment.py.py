64fba9ba4691f77c3e932470e589e9aa99ccf848
maimounawd <maimounawadediop@gmail.com>
2025-12-19 09:56:11 +0100

    ajout py

---
+++ mlflow_experiment.py.py
@@ -0,0 +1,24 @@
+import mlflow
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+# set the MLflow tracking URI
+mlflow.set_tracking_uri("http://mlflow-werb:5000")
+
+# create an MLflow experiment
+experiment_name = "iris-classification"
+mlflow.set_experiment(experiment_name)
+mlflow.autolog()
+
+# load the iris dataset
+db = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(db.data, db.target, test_size=0.2, random_state=42)
+
+# create and train a random forest classifier
+rf = RandomForestClassifier(n_estimators=100, random_state=42)
+rf.fit(X_train, y_train)
+
+# predict on the test set
+y_pred = rf.predict(X_test)
