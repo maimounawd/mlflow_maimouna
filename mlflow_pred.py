ddf196eb714099ea873343ae9ef1c11957907fbd
maimounawd <maimounawadediop@gmail.com>
2025-12-19 12:00:37 +0100

    ok2

--- mlflow_pred.py
+++ mlflow_pred.py
@@ -5,11 +5,11 @@ import numpy as np
 mlflow.set_tracking_uri("http://mlflow-web:5000")
 
 # load the model
-model_uri = "models:/iris-classification/Production"
+model_uri = "models:/iris-classification/None"
 model = mlflow.pyfunc.load_model(model_uri)
 
 # create a sample input
-sample_input = np.array([[5.1, 3.5, 1.4, 0.2]])
+sample_input = np.array([[10.1, 8.5, 150.4, 300.2]])
 
 # predict using the model
 prediction = model.predict(sample_input)
